ad6d128b9a49563fa02ae329753693e6b2796524
Осипян Артем <artemosipyan111@gmail.com>
2025-06-27 00:22:18 +0300

    добавил файл readme.md с документацией

--- client.py
+++
@@ -1,9 +0,0 @@
-import httpx
-
-while True:
-    expr = input("Введите выражение (или 'exit'): ")
-    if expr == "exit":
-        break
-
-    response = httpx.post("http://localhost:8000/calculate", json={"expression": expr})
-    print("Ответ:", response.json())
